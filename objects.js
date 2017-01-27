d1e549460d9230beb2ba5f14081db25205ff8d3b
chriscast88 <christian.castaneda@gmail.com>
2017-01-26 20:46:52 -0800

    commit

--- objects.js
+++ objects.js
@@ -1,1 +1,48 @@
-var 
+var person = {
+  firstName: 'Alex',
+  lastName: 'Penman',
+  glasses: true,
+  hairColor: 'black'
+};
+
+
+var classGroup = [
+  {
+    firstName: 'Alex',
+    glasses: true,
+    hairColor: 'black'
+  },
+  {
+    firstName: 'Christian',
+    glasses: false,
+    hairColor: 'black'
+  },
+  {
+    firstName: 'Charlie',
+    glasses: false,
+    hairColor: 'auburn'
+  },
+  {
+    firstName: 'Joshua',
+    glasses: false,
+    hairColor: 'black'
+  }
+];
+
+
+
+var describeGroup = function (group){
+
+  for ( var i =0; i<group.length; i++){
+    if(group[i].glasses === true){
+      var hasGlasses = 'does';
+    } else {
+      var hasGlasses = 'does not';
+      }
+
+    console.log(group[i].firstName + ' has ' + group[i].hairColor + ' hair and ' + hasGlasses + ' wear glasses.');
+  }
+}
+
+
+describeGroup(classGroup);
